f6d2b9f1fb30ef298e706aa2ab737afe0181e1b7
Jacob Stevens <J.stevensOne@gmail.com>
2020-12-11 00:12:26 -0500

    Finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,22 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var element = document.body
+  var result = [];
+
+
+  var hasClassName = function(element) {
+    if(element.classList && element.classList.contains(className)){
+      result.push(element);
+    }
+  if(element.hasChildNodes()){
+    for (var i = 0; i < element.childNodes.length; i++) {
+      hasClassName(element.childNodes[i]);
+    }
+  };
+}
+
+  hasClassName(element);
+
+  return result;
 };
